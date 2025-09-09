91307ad73c6560eb18c30dc9057b84c330389f78
ZairaStra <straticozaira@gmail.com>
2025-09-09 10:15:25 +0200

    add Artist repo

---
+++ capstone_be/src/main/java/zairastra/capstone_be/repositories/Artist.java
@@ -0,0 +1,16 @@
+package zairastra.capstone_be.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import zairastra.capstone_be.entities.enums.Genre;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface Artist extends JpaRepository<Artist, Long> {
+
+    Optional<Artist> findByNameIgnoreCase(String name);
+
+    List<Artist> findByNameContainingIgnoreCase(String name);
+
+    List<Artist> findByGenre(Genre genre);
+}
