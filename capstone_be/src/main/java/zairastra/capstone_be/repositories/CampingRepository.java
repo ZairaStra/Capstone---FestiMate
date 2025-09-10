717eeb67be696c1b9f216ee5ac49191bc5d8a472
ZairaStra <straticozaira@gmail.com>
2025-09-10 18:25:33 +0200

    added FestivalRepository, CampingRepository and CampingUNitRepository (and little fixes)

--- capstone_be/src/main/java/zairastra/capstone_be/repositories/CampingRepository.java
+++ capstone_be/src/main/java/zairastra/capstone_be/repositories/CampingRepository.java
@@ -3,7 +3,13 @@ package zairastra.capstone_be.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import zairastra.capstone_be.entities.Camping;
+import zairastra.capstone_be.entities.Festival;
+
+import java.util.Optional;
 
 @Repository
 public interface CampingRepository extends JpaRepository<Camping, Long> {
+
+    Optional<Camping> findByFestival(Festival festival);
+
 }
--- capstone_be/src/main/java/zairastra/capstone_be/repositories/FestivalRepository.java
+++ capstone_be/src/main/java/zairastra/capstone_be/repositories/FestivalRepository.java
@@ -4,20 +4,30 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import zairastra.capstone_be.entities.Festival;
 
-import java.time.LocalDate;
 import java.util.Optional;
 
 public interface FestivalRepository extends JpaRepository<Festival, Long>, JpaSpecificationExecutor<Festival> {
 
     Optional<Festival> findByNameIgnoreCase(String name);
 
-    Page<Festival> findByNameStartingWithIgnoreCase(String name, Pageable pageable);
+//    Page<Festival> findByNameStartingWithIgnoreCase(String name, Pageable pageable);
+//
+//    Page<Festival> findByCityStartingWithIgnoreCase(String city, Pageable pageable);
+//
+//    Page<Festival> findByCountryStartingWithIgnoreCase(String country, Pageable pageable);
+//
+//    Page<Festival> findByArtists_NameStartingWithIgnoreCase(String prefix, Pageable pageable);
+//
+//    Page<Festival> findByArtists_Genre(Genre genre, Pageable pageable);
+//
+//    Page<Festival> findByStartDateBetween(LocalDate start, LocalDate end, Pageable pageable);
+//
+//    Page<Festival> findByEndDateBetween(LocalDate start, LocalDate end, Pageable pageable);
 
-    Page<Festival> findByGenre(String genre, Pageable pageable);
-
-    Page<Festival> findByDateBetween(LocalDate start, LocalDate end, Pageable pageable);
-
-    Page<Festival> findByArtists_NameStartingWithIgnoreCase(String prefix, Pageable pageable);
+    @Query("SELECT f FROM PublicUser u JOIN u.wishlist f WHERE u.id = :userId")
+    Page<Festival> findWishlistByUserId(Long userId, Pageable pageable);
 }
+
