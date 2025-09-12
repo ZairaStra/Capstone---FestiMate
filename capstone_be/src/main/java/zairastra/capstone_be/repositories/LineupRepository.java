7e47026131803662846685e30aa61e43a463c230
ZairaStra <straticozaira@gmail.com>
2025-09-12 09:01:28 +0200

    added boolen controls

--- capstone_be/src/main/java/zairastra/capstone_be/repositories/LineupRepository.java
+++ capstone_be/src/main/java/zairastra/capstone_be/repositories/LineupRepository.java
@@ -6,12 +6,22 @@ import zairastra.capstone_be.entities.Artist;
 import zairastra.capstone_be.entities.Festival;
 import zairastra.capstone_be.entities.Lineup;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 
 @Repository
 public interface LineupRepository extends JpaRepository<Lineup, Long> {
 
     List<Lineup> findByFestival(Festival festival);
-    
+
     List<Lineup> findByArtist(Artist artist);
+
+    boolean existsByFestival(Festival festival);
+
+    boolean existsByArtistAndDateAndStartTime(
+            Artist artist,
+            LocalDate date,
+            LocalTime startTime
+    );
 }
