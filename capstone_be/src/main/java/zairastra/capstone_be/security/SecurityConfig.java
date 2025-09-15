1baf36f592f9b9c26d59dd01e529030b6795d8ad
ZairaStra <straticozaira@gmail.com>
2025-09-15 10:41:48 +0200

    added CORS

--- capstone_be/src/main/java/zairastra/capstone_be/security/SecurityConfig.java
+++ capstone_be/src/main/java/zairastra/capstone_be/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package zairastra.capstone_be.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -10,6 +11,12 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -26,12 +33,27 @@ public class SecurityConfig {
                         .requestMatchers("/admins/**").hasRole("SYSTEM_ADMIN")
                         .anyRequest().authenticated()))
                 .addFilterBefore(jwtCheckerFilter, UsernamePasswordAuthenticationFilter.class);
+
+        httpSecurity.cors(Customizer.withDefaults());
+
         return httpSecurity.build();
     }
 
     @Bean
     public PasswordEncoder getBCrypt() {
         return new BCryptPasswordEncoder(12);
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3002"));
+        configuration.setAllowedMethods(List.of("*"));
+        configuration.setAllowedHeaders(List.of("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 }
