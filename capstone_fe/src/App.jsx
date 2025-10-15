ca6d9fc399ef230e7b2f2a38f287b234922bdd44
ZairaStra <straticozaira@gmail.com>
2025-10-15 17:17:46 +0200

    fixed location

--- capstone_fe/src/App.jsx
+++ capstone_fe/src/App.jsx
@@ -2,7 +2,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "./App.css";
 
-import API_URL from "../../config/api";
+import API_URL from "./config/api";
 
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { useState, useEffect } from "react";
